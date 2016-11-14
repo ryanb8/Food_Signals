e85ee14536a61ffc2274a2b984106fa61f7ac7c0
Ryan Boyer <Ryan@d-172-25-45-205.dhcp.virginia.edu>
2016-11-14 15:38:44 -0500

    Feat: Add globals for HTML tags for parsing

--- scrape_bao.py
+++ scrape_bao.py
@@ -11,19 +11,76 @@ scrape out the data, transform it into standard form, and export to .csv
 Changelog:      Initial Version 2016 11 14
 """
 
-from bs4 import BeautifulSoup as bs4
 import os
 import pandas as pd
-import numpy as np
+import bs4
+
+# Globals
+field_tags = ["Name", "Street", "City", "State_Name", "Zip", "NAICS", "SIC",
+              "Employee_Number", "Sales_Volume", "Latitude", "Longitude",
+              "Object_ID", "Description"]
+field_tag_dict = {"Name": ("dgrid-cell dgrid-cell-padding "
+                           "dgrid-column-field1 field-CONAME field1"),
+                  "Street": ("dgrid-cell dgrid-cell-padding "
+                             "dgrid-column-field2 field-STREET field2"),
+                  "City": ("dgrid-cell dgrid-cell-padding "
+                           "dgrid-column-field3 field-CITY field3"),
+                  "State_Name": ("dgrid-cell dgrid-cell-padding "
+                                 "dgrid-column-field4 field-STATE_NAME field4"),
+                  "Zip": ("dgrid-cell dgrid-cell-padding "
+                          "dgrid-column-field5 field-ZIP field5"),
+                  "NAICS": ("dgrid-cell dgrid-cell-padding "
+                            "dgrid-column-field6 field-NAICS field6"),
+                  "SIC": ("dgrid-cell dgrid-cell-padding "
+                          "dgrid-column-field7 field-SIC field7"),
+                  "Employee_Number": ("dgrid-cell dgrid-cell-padding "
+                                      "dgrid-column-field8 field-EMPNUM "
+                                      "field8"),
+                  "Sales_Volume": ("dgrid-cell dgrid-cell-padding "
+                                   "dgrid-column-field9 field-SALESVOL field9"),
+                  "Latitude": ("dgrid-cell dgrid-cell-padding "
+                               "dgrid-column-field11 field-LATITUDE field11"),
+                  "Longitude": ("dgrid-cell dgrid-cell-padding "
+                                "dgrid-column-field12 field-LONGITUDE field12"),
+                  "Object_ID": ("dgrid-cell dgrid-cell-padding "
+                                "dgrid-column-field0 field-OBJECTID field0"),
+                  "Description": ("dgrid-cell dgrid-cell-padding "
+                                  "dgrid-column-field10 field-DESC_ field10")
+              }
 
 
 def import_html_file(path):
+    """
+    :param path:
+    :return:
+    """
     if os.path.isfile(path=path):
         with open(path, mode='r') as f:
             lines = f.read()
         return lines
     else:
         return False
+
+
+def get_data(tag, files):
+    """
+    Takes each file in the files list, scrapes BAO data out of it
+    and transforms into a single pandas DF.
+
+    :param tag: type of tag, as string
+    :param files: list of file paths
+    :return: pandas dataframe, error flag
+    """
+    data = pd.dataframe()
+    error_flag = False
+    error_files = []
+    for file in files:
+        if os.path.isfile(file):
+            soup = bs4.BeautifulSoup(open(file), "lxml")
+        else:
+            error_flag = True
+            error_files.append(file)
+    return (data, error_flag, error_files)
 
 
 def main():
@@ -55,11 +112,7 @@ def main():
     for key in doc_tags:
         tag = key
         files = doc_tags[key]
-        data = get_data(tag, files)
-
-
-
-
+        (data, error_flag, error_files) = get_data(tag, files)
 
 
 if __name__ == "__main__":
