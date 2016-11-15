21c0002298c74c286935bf36a267dffb551a89d9
Ryan Boyer <Ryan@d-172-25-44-233.dhcp.virginia.edu>
2016-11-15 15:05:14 -0500

    Sort by Name ascending (a to z), sales descending (big to small)

--- scrape_bao.py
+++ scrape_bao.py
@@ -93,7 +93,7 @@ def get_data(files):
     data = data[field_tags]
     data = data.sort_values(by=[field_tags[0], field_tags[8]],
                             axis=0,
-                            ascending=[True, True])
+                            ascending=[True, False])
     return data, error_flag, error_files
 
 
