5c4f73532e9234086481ebb82250e0c426ba0cb3
Ryan Boyer <Ryan@d-172-25-44-233.dhcp.virginia.edu>
2016-11-15 14:42:07 -0500

    Scrape and Write data to File

--- scrape_bao.py
+++ scrape_bao.py
@@ -131,7 +131,7 @@ def main():
     for i in range(len(all_data)):
         fpath = "/Users/Ryan/Desktop/bao" + str(i) + ".csv"
         all_data[i].to_csv(path_or_buf=fpath,
-                              sep=",")
+                           sep=",")
 
 if __name__ == "__main__":
     main()
