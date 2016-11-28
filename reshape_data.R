386786c2808193bc68f52edf19c821111962fcca
Ryan Boyer <Ryan@d-172-25-44-100.dhcp.virginia.edu>
2016-11-28 12:02:59 -0500

    Clean and organize restaurant and grocery store data

--- reshape_data.R
+++ reshape_data.R
@@ -2,15 +2,42 @@
 # This script reshapes all data into appropriate shape and format for analysis.
 
 # Packages
+library(dplyr)
 
 
 # FULL_DATA
+
 
 # Type conversions
 
 # Nested List formation
 
 # Grocery and restaurant Data
+
+clean_gsr <- function(df, drops, chain_name){
+  #df is data frame
+  #drops is vector of column names to be dropped
+  #chain_name is the column name of the column providing the store name
+  
+  #Drop
+  df <- df[ , !(names(df) %in% drops)]
+  
+  #Create unique_name
+  # Removes punc&spaces, all lowercases
+  df$chain_id <- tolower(gsub("[[:punct:]\ ]",
+                              "",
+                              df[ , chain_name]))
+  
+  #Reorder (chain Id is first column,
+  #rows sorted alphabetically by chain_ID)
+  df <- df %>%
+    select(chain_id, everything()) %>%
+    arrange(chain_id)
+                      
+  return(df)
+}
+
+# Remove 
 
 
 #????
