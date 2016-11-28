f03f7cbf1f6eabe328bb62f509e75dbe14afd172
Ryan Boyer <Ryan@d-172-25-44-100.dhcp.virginia.edu>
2016-11-28 11:25:05 -0500

    Outline Project

--- final_proj_analysis.R
+++ final_proj_analysis.R
@@ -1,2 +1,14 @@
-#PLAN5120 FINAL PROJECT ANALYSIS
-#This 
+# PLAN5120 FINAL PROJECT ANALYSIS
+# This script performs the analysis for my final project for PLAN5120
+# It imports the data created by ARCGIS, reshapes it, performs weighted
+# averages on the calculations, and generates graphs.
+
+#Import data
+
+#Reshape and Organize Data
+
+#Filter Data
+
+#Statistics
+
+#Graphs
