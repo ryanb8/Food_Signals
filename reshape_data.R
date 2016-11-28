76574f453bb3bdb0ff34b6bed62d65eb61d6db39
Ryan Boyer <Ryan@d-172-25-44-100.dhcp.virginia.edu>
2016-11-28 11:31:50 -0500

    Outline Reshape

--- reshape_data.R
+++ reshape_data.R
@@ -1,2 +1,16 @@
 # Reshape Data
 # This script reshapes all data into appropriate shape and format for analysis.
+
+# Packages
+
+
+# FULL_DATA
+
+# Type conversions
+
+# Nested List formation
+
+# Grocery and restaurant Data
+
+
+#????
