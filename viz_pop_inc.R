cfdb91f86a78924d3c6328644065472b23f5413b
Ryan Boyer <Ryan@d-172-25-45-12.dhcp.virginia.edu>
2016-11-29 10:59:58 -0500

    Version 1 of plot complete - able to manually change buffer range.

--- viz_pop_inc.R
+++ viz_pop_inc.R
@@ -1,2 +1,28 @@
 #Vizualize population vs income
+library(ggplot2)
+
+#group stores by buffer
+#not actually using dplyr group, as making graphs of groups is difficult
+stores_buf_order <- arrange(stores, buffer, desc(type)) 
+
+#make plot
+dfp <- stores_buf_order[which(stores_buf_order$buffer == "1.5 Miles"), ]
+#G vs R
+pop_inc_plot <- ggplot(dfp,aes(prop_pop, per_cap_inc_w)) +
+  geom_point(aes(colour = type), size = 1) +
+  ggtitle(paste0("Population and Per Capita Income 
+for Restaurants and Grocery Stores 
+in Buffer Range of ", dfp[1, "buffer"])) + 
+  labs(x = "Est. Population in Service Area", y = "Weighted Per Capita Income ($)")
+pop_inc_plot
+
+#Count
+pop_inc_plot <- ggplot(dfp,aes(prop_pop, per_cap_inc_w)) +
+  geom_point(aes(colour = type), size = 1) +
+  ggtitle(paste0("Population and Per Capita Income 
+for Restaurants and Grocery Stores 
+in Buffer Range of ", dfp[1, "buffer"])) + 
+  labs(x = "Est. Population in Service Area", y = "Weighted Per Capita Income ($)")
+pop_inc_plot
+
 
