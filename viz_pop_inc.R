7aace825987c5ec0a3f6d35d5af7a9b4be840f75
Ryan Boyer <Ryan@ryan-boyers-macbook-pro.local>
2016-11-29 21:08:30 -0500

    create and export animation for FP

--- viz_pop_inc.R
+++ viz_pop_inc.R
@@ -7,6 +7,8 @@ library(gganimate)
 #group stores by buffer
 #not actually using dplyr group, as making graphs of groups is difficult
 stores_buf_order <- arrange(stores2, buffer, desc(type)) 
+stores_buf_order$type[which(stores_buf_order$type == "G")] <- "Grocery Store"
+stores_buf_order$type[which(stores_buf_order$type == "R")] <- "Restaurant"
 
 buf_vals <- c("0.25 Miles", "0.75 Miles", "1.5 Miles", "3 Miles", "5 Miles")
 
@@ -24,10 +26,33 @@ pop_inc_plot
 
 pop_inc_plot <- ggplot(stores_buf_order, aes(prop_pop, per_cap_inc_w)) +
   geom_point(aes(colour = type, frame=buffer), size = 1.25) +
-  ggtitle("Population and Per Capita Income for Food Service Establishments \nBuffer Range:\n\t ") +
+  ggtitle(paste0(c(
+    "Population and Per Capita Income",
+    "for Food Service Establishments",
+    "Buffer Range: "), collapse ="\n"))+
   labs(x = "Log Est. Population in Service Area", 
        y = "Weighted Per Capita Income ($)") +
-  scale_x_log10()
+  scale_x_log10() +
+  guides(colour = guide_legend(
+    title="Store Type",
+    size = 15,
+    face = "bold"
+  )) + 
+  labs(caption = paste0(c(
+    "Establishment data from ESRI Business Analyst Online, November 2016",
+    "Economic and population data from the 2010-2014 ACS 5 Year Summary",
+    "Census tract boundaries from the US Census Bureau, November 2016",
+    "All establishments are within the 10 counties in the Atlanta Regional Commission",
+    "Processed in Python using ArcPy, Generated in R using ggplot2 and gganimiate",
+    "Created by Ryan Boyer on 11/29/16"
+  ), collapse ="\n")) +
+  theme(plot.caption = element_text(size = rel(0.8), hjust=0.5),
+        plot.title = element_text(size = rel(1.5), hjust=0.5),
+        axis.title = element_text(size = rel(1.2)))
+gganimate::gg_animate(pop_inc_plot)
+gganimate::gg_animate(pop_inc_plot,
+                      filename = "/Users/Ryan/Desktop/pop_inc_fs.gif",
+                      saver = "gif")
 
 #Count
 dfp <- stores_buf_order[which(stores_buf_order$buffer == buf_vals[4]), ]
